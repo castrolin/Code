0d2083950c07a54af914fb146ee44d610f12f766
castrolin <castrolin0120@gmail.com>
2021-03-23 09:48:08 +0800

    The searching the centroid of melt pool

--- Meltpool_Centroid.py
+++ Meltpool_Centroid.py
@@ -10,6 +10,7 @@ import imutils
 import time
 import pylab
 import os
+import matplotlib.pyplot as plt
 
 # initialize the list of tracked points, the frame counter
 # and the coordinate deltas
@@ -20,7 +21,9 @@ directio = ""
 #######################################
 # read the video file
 path = "A4.avi"
-vid = imageio.get_reader(path,'ffmpeg')
+vid = cv2.VideoCapture(path)
+NumOfFrame = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
+print(NumOfFrame)
 ########################################
 '''
 Use Opencv to cover the imageio
@@ -29,42 +32,49 @@ the main target:
     2.the fram
 '''
 count = 0
-print(vid.shape)
-for num in range(vid):
+for num in range(NumOfFrame):
     try:
-        img = vid.get_data(num)
-        img = cv2.resize(img,(256,128))
+        _,img = vid.read(num)
         gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
         _,im = cv2.threshold(gray_img,220,1,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
-        image, cnts, hier = cv2.findContours(im,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-        center = None
-        for c in cnts:
-            x,y,w,h = cv2.boundingRect(c)
-            cv2.rectangle(img, (x,y),(x+w,y+h),(0,255,0),2)
+        if sum(sum(gray_img.astype('double'))) > 1000:
+            cnts, hier = cv2.findContours(im,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+            center = None
+            for c in cnts:
+                x,y,w,h = cv2.boundingRect(c)
+                cv2.rectangle(img, (x,y),(x+w,y+h),(0,255,0),2)
 
-            rect = cv2.minAreaRect(c)
-            box = cv2.boxPonts(rect)
+                rect = cv2.minAreaRect(c)
+                box = cv2.boxPoints(rect)
 
-            box = np.int0(box)
-            cv2.drawContours(img, [box],0, (0,0,255))
-        if len(cnts)>1:
-            c = sorted(cnts, key= cv2.contourArea)[-2]
-            ((x,y),radius) = cv2.minEnclosingCircle(c)
-            M = cv2.moments(c)
-            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-            cv2.circle(img, (int(x),int(y)),int(radius),(0,255,255),2)
-            cv2.circle(img, center,5,(0,0,255),-1)
-            pts.append(center)
-        cv2.imshow('img',img)
+                box = np.int0(box)
+                cv2.drawContours(img, [box],0, (0,0,255))
+            if len(cnts)>1:
+                c = sorted(cnts, key= cv2.contourArea)[-2]
+                ((x,y),radius) = cv2.minEnclosingCircle(c)
+                M = cv2.moments(c)
+            #deal with division by zero
+                if M["m00"] != 0:
+                    center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+                else:
+                    center = (0,0)
+
+                cv2.circle(img, (int(x),int(y)),int(radius),(0,255,255),2)
+                cv2.circle(img, center,5,(0,0,255),-1)
+                pts.append(center)
+    except :
+        print('Error or something happend')
+# write the figure into video and turn
+'''                
         fourcc = cv2.VideoWriter_fourcc('F','M','P','4')
         out = cv2.VideoWriter('output.avi',-1,20.0,(640,480))
         pylab.imsave("images/"+"image-"+str(num)+".jpg",im)
     except :
-        print('Error')
+        print('Error', num)
 
 os.system('ffmpeg -framerate 25 -i images/image-%00d.jpg -r 76 -s 800x600 output.avi')
-
+'''
 
 
 
